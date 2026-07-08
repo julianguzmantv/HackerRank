3eb14c77ffd515f8dc6769ca526e2b11605dd394
Julian Guzmán <julianguzmantv@gmail.com>
2026-07-08 16:59:24 -0500

    Refactor JavaHashSet to use Solution class and improve input
    Solving the Test 5 Case problem.

--- Java/Collections/JavaHashSet.java
+++ Java/Collections/JavaHashSet.java
@@ -49,21 +49,28 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class JavaHashSet 
-{
+public class Solution {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
 
-        HashSet  hs = new HashSet ();
-        int count = 0;
-        Scanner sc = new Scanner(System.in);
-        int numCases = Integer.parseInt(sc.nextLine());
-        for(int i = 0;i < numCases;i++){
-            if(hs.add(sc.nextLine())){
-                count++;
-            }
-            System.out.println(count);
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < t; i++) {
+            String key = (pair_left[i].compareTo(pair_right[i]) <= 0 ? 
+                         (pair_left[i] + " " + pair_right[i]) : 
+                         (pair_right[i] + " " + pair_left[i]));
+            set.add(key);
+            System.out.println(set.size());
         }
         
+        s.close();
     }
 }
